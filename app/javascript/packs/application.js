daacc9e267781ae91f214996748b96852a83c72e
seiichi621 <sei-0941-seiichi5@softbank.ne.jp>
2020-12-23 23:51:25 +0900

    Revert "投稿機能"
    This reverts commit 2b46052cc445c6f13cc929ec7ef951098568e41a.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require bootstrap-sprockets
-//= require_tree .
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
